a579c06a48e6f8590154b8dfa072ce797a4b1e70
kathnt <silver.kdragon@gmail.com>
2022-06-14 23:14:17 -0400

    Fully working up to before db

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+from .views import search
+
 app_name = "myapp"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', search,name="search"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 import openpyxl
+from django.http import HttpResponse
+from django.views.generic import ListView
 
 def index(request):
     if "GET" == request.method:
@@ -32,11 +34,21 @@ def index(request):
         # getting value from each cell in row
         for row in worksheet.iter_rows():
             row_data = list()
+            temp = 0
             for cell in row:
+                
                 value = ""
                 if cell.value is not None:
                     value = str(cell.value)
- 
+                    if temp == 2:
+                      value = value + 'second' 
+                      ### CHECK VALUE INSIDE DATABASE ####################
+                      # if in database:
+                      
+                      # temp test REPLACE LATER WITH 'IF IN DATABASE:'
+                      if "鋼" in value:
+                          value = value + '!'
+                temp += 1
                 row_data.append(value)
                 #print(cell.value)
             excel_data.append(row_data)
@@ -44,10 +56,25 @@ def index(request):
         return render(request, 'myapp/index.html', {"excel_data":excel_data})
 
 
+def search(request):
+    if "GET" == request.method:
+        return render(request, 'myapp/results.html', {})
+    else:
+        result_list = list()
+           
+        form = request.POST
+        #print(form['results_btn'])
+        
+        keyword_str = form['results_btn']
+        print(keyword_str)
+
+        #test!!!!!!!!!!!!
+        result_list.append('apple')
+        result_list.append('book')
+        result_list.append('choo choo train')
+        result_list.append(keyword_str)
 
 
+        return render(request, 'myapp/results.html', {"result_list":result_list})
 
-
-
-
-
+ 
